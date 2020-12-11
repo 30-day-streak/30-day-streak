8bc734cef3cefe49eeb09c644d375fd9d18d5eb7
Katie Gracey <gracey.kd@gmail.com>
2020-12-11 12:50:19 +0100

    create challenge with title and goal

--- client/src/components/CreateChallenge.js
+++ client/src/components/CreateChallenge.js
@@ -1,2 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
+
+export default class CreateChallenge extends Component {
+  state = {
+    title: '',
+    goal: '',
+    dailyTargetDescription: '',
+    dailyTargetNumber: '',
+    dailyTargetUnit: '',
+    category: '',
+    // private: '',
+  };
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(this.state);
+    console.log(this.state);
+    axios
+      .post('/challenges', {
+        title: this.state.title,
+        goal: this.state.goal,
+        // dailyTarget: {
+        //   description: this.state.dailyTargetDescription,
+        //   number: this.state.dailyTargetNumber,
+        //   unit: this.state.dailyTargetUnit,
+        // },
+        // category: this.state.category,
+        // owner: owner
+        // private: false
+      })
+      .then(() => {
+        // set the form to it's initial state (empty input fields)
+        this.setState({
+          title: '',
+          goal: '',
+          // dailyTargetDescription: '',
+          // dailyTargetNumber: '',
+          // dailyTargetUnit: '',
+          // category: '',
+          // private: '',
+        });
+        // update the parent components state (in Projects) by calling getData()
+        // this.props.getData();
+      });
+    // .catch(err => console.log(err))
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="goal">Goal</label>
+          <input
+            type="text"
+            id="goal"
+            name="goal"
+            value={this.state.goal}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add a Project</button>
+        </form>
+      </>
+    );
+  }
+}
--- routes/challenges.js
+++ routes/challenges.js
@@ -1,7 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Challenge = require ('../models/Challenge')
 
 //here
-
+router.post('/', (req, res) => {
+  console.log('test');
+  console.log('req:', req.body);
+  const { title, goal } = req.body;
+  // const owner = req.user._id
+  // console.log('owner', owner);
+  Challenge.create({
+    title,
+    goal,
+    // owner
+  })
+    // // Get the data and return the project data as json
+    // // best practice to send http code so that the client knows what's happening
+    // .then(res.status(201));
+  .then(challenge => {
+    console.log('sucessful add');
+    res.status(201).json(challenge)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+});
 
 module.exports = router;
