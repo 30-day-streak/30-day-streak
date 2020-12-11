d39b479843b3e7df15cd9f80da2bd259bc29d44b
Katie Gracey <gracey.kd@gmail.com>
2020-12-11 13:24:27 +0100

    create challenge with all MVP data

--- routes/challenges.js
+++ routes/challenges.js
@@ -5,7 +5,7 @@ const Challenge = require ('../models/Challenge')
 //here
 router.post('/', (req, res) => {
   // console.log('test');
-  console.log('req user:', req.user._id);
+  // console.log('req user:', req.user._id);
   const { title, goal, category} = req.body;
   const {description, number, unit} = req.body.dailyTarget
   const owner = req.user._id
