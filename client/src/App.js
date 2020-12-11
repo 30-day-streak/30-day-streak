1b1d897fb50d685382581c733c7aac7de3be6545
“Anna <aniawaszak@yahoo.com>
2020-12-11 11:55:44 +0100

    add sign up, login, logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SignUp from './components/auth/SignUp';
+import Login from './components/auth/Login';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({
+      user: user
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar user={this.state.user} setUser={this.setUser} />
+        <Switch>
+          <Route
+            exact
+            path='/signup'
+            render={props => <SignUp setUser={this.setUser} {...props} />}
+          />
+          <Route
+            exact
+            path='/login'
+            render={props => <Login setUser={this.setUser} {...props} />}
+          />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
