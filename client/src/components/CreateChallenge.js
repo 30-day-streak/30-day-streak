d27ae1454f3d3caef315613c0c4cf5c52b6e5d7b
Katie Gracey <gracey.kd@gmail.com>
2020-12-14 15:07:25 +0100

    create new challenge and add to user model

--- client/src/components/CreateChallenge.js
+++ client/src/components/CreateChallenge.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class CreateChallenge extends Component {
   state = {
@@ -13,7 +13,7 @@ export default class CreateChallenge extends Component {
   };
 
   handleChange = (event) => {
-    console.log(event.target.name);
+    // console.log(event.target.name);
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
@@ -21,98 +21,141 @@ export default class CreateChallenge extends Component {
     });
   };
 
-  handleSubmitLater = (event) => {
+  // handleSubmitLater = (event) => {
+  //   event.preventDefault();
+  //   // console.log('success');
+  //   // console.log('state from create challenge',this.state);
+  //   let id = this.state.user._id;
+  //   axios
+  //     .post('/challenges', {
+  //       title: this.state.title,
+  //       goal: this.state.goal,
+  //       dailyTarget: {
+  //         description: this.state.dailyTargetDescription,
+  //       },
+  //       category: this.state.category,
+  //     })
+  //     .then((res) => {
+  //       let user = this.props.user;
+  //       user.challenges.push({
+  //         id: res.data._id,
+  //         status: 'favorite',
+  //       });
+  //       this.setState({
+  //         user: this.props.user,
+  //       });
+  //       console.log('state', this.state.user);
+  //     })
+  //     .then(
+  //       axios.put(`/users/${id}`), {
+  //       user: this.state.user,
+  //     })
+  //     .then((res) => {
+  //       this.props.history.push('/');
+  //     });
+  //   // console.log('state', this.state);
+  //   // })
+  // };
+
+  handleSubmitLater = async (event) => {
     event.preventDefault();
-    console.log('success');
-    console.log(this.state);
-    let id = this.state.user._id
-    axios
-      .post('/challenges', {
+    console.log('user before post', this.state.user);
+    let id = this.state.user._id;
+    try {
+      const newChallenge = await axios.post('/challenges', {
         title: this.state.title,
         goal: this.state.goal,
         dailyTarget: {
           description: this.state.dailyTargetDescription,
         },
         category: this.state.category,
-      })
-      .then(res => {
-        let user = this.props.user
-        let newUser = user.challenges.push({
-          id: res.data._id,
-          status: 'favorite'
-        })
-        this.setState({
-          user: newUser
-          })
-        })
-        .then(axios.put(`/users/${id}`), {
-          user: this.state.user
-        }).then(res => {
-          this.props.history.push('/');
-        })
-        // console.log('state', this.state);
-      // })
-  };
-          
+      });
 
-  componentDidMount () {
+      let user = this.props.user;
+      console.log('user', this.props.user);
+      user.challenges.push({
+        id: newChallenge.data._id,
+        status: 'favorite',
+      });
+      this.setState({
+        user: this.props.user,
+      });
+      console.log('user after set state', this.state.user);
+
+      const updatedUser = await axios.put(`/users/${id}`, {
+        user: this.state.user,
+      });
+      // console.log(updatedUser.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
     this.setState({
-      user: this.props.user
-    })
+      user: this.props.user,
+    });
   }
 
   render() {
+    // console.log(this.props.user);
     return (
       <>
-      <div className="create-challenge-page">
-        <form className="create-challenge-form" id="create-challenge-form" onSubmit={this.handleSubmitLater}>
-        <h1>Create a Challenge</h1>
-        {/* <h3>Here you can make your own customized challenge</h3> */}
-        <div className="create-challenge-form-item">
-          <label htmlFor="title">Name your challenge: </label>
-          <br/>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-            placeholder="Example: Get More Sleep"
-            required
-          />
-          </div>
+        <div className="create-challenge-page">
+          <form
+            className="create-challenge-form"
+            id="create-challenge-form"
+            onSubmit={this.handleSubmitLater}
+          >
+            <h1>Create a Challenge</h1>
+            {/* <h3>Here you can make your own customized challenge</h3> */}
+            <div className="create-challenge-form-item">
+              <label htmlFor="title">Name your challenge: </label>
+              <br />
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChange}
+                placeholder="Example: Get More Sleep"
+                required
+              />
+            </div>
 
-          <div className="create-challenge-form-item">
-          <label htmlFor="goal">What do you want to accomplish with this challenge?</label>
-          <br/>
-          <input
-            type="text"
-            id="goal"
-            name="goal"
-            value={this.state.goal}
-            onChange={this.handleChange}
-            placeholder="Example: Improve my sleep habits so I feel more rested and alert throughout the day"
-            required
-          />
-          </div>
+            <div className="create-challenge-form-item">
+              <label htmlFor="goal">
+                What do you want to accomplish with this challenge?
+              </label>
+              <br />
+              <input
+                type="text"
+                id="goal"
+                name="goal"
+                value={this.state.goal}
+                onChange={this.handleChange}
+                placeholder="Example: Improve my sleep habits so I feel more rested and alert throughout the day"
+                required
+              />
+            </div>
 
-          <div className="create-challenge-form-item">
-          <label htmlFor="dailyTargetDescription">
-            What will you do every day to meet this goal?
-          </label>
-          <br/>
-          <input
-            type="text"
-            id="dailyTargetDescription"
-            name="dailyTargetDescription"
-            value={this.state.dailyTargetDescription}
-            onChange={this.handleChange}
-            placeholder="Example: Get 8 hours of sleep each night"
-            required
-          />
-          </div>
+            <div className="create-challenge-form-item">
+              <label htmlFor="dailyTargetDescription">
+                What will you do every day to meet this goal?
+              </label>
+              <br />
+              <input
+                type="text"
+                id="dailyTargetDescription"
+                name="dailyTargetDescription"
+                value={this.state.dailyTargetDescription}
+                onChange={this.handleChange}
+                placeholder="Example: Get 8 hours of sleep each night"
+                required
+              />
+            </div>
 
-          {/* <div className="create-challenge-form-item">
+            {/* <div className="create-challenge-form-item">
           <label htmlFor="dailyTargetNumber">Daily Target Number</label>
           <br/>
           <input
@@ -136,27 +179,31 @@ export default class CreateChallenge extends Component {
           />
           </div> */}
 
-          <div className="create-challenge-form-item create-challenge-form-select">
-          <label htmlFor="category">What category does your challenge fall into?</label>
-          <br/>
-          <select
-            name="category"
-            id="category"
-            value={this.state.category}
-            onChange={this.handleChange}
-            required
-          >
-            <option value="eat">Eat</option>
-            <option value="train">Train</option>
-            <option value="habit">Habit</option>
-            <option value="skill">Skill</option>
-            <option value="other">Other</option>
-          </select>
-          </div>
+            <div className="create-challenge-form-item create-challenge-form-select">
+              <label htmlFor="category">
+                What category does your challenge fall into?
+              </label>
+              <br />
+              <select
+                name="category"
+                id="category"
+                value={this.state.category}
+                onChange={this.handleChange}
+                required
+              >
+                <option value="eat">Eat</option>
+                <option value="train">Train</option>
+                <option value="habit">Habit</option>
+                <option value="skill">Skill</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
 
-          <button type="submit" form="create-challenge-form" >Save challenge for later</button>
-          {/* <button type="submit" form="create-challenge-form" onSubmit={this.handleSubmitStart}>Start challenge now!</button> */}
-        </form>
+            <button type="submit" form="create-challenge-form">
+              Save challenge for later
+            </button>
+            {/* <button type="submit" form="create-challenge-form" onSubmit={this.handleSubmitStart}>Start challenge now!</button> */}
+          </form>
         </div>
       </>
     );
