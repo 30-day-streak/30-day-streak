da5ed4c007efe5cf52d9c30b524c24d0c3912a01
“Anna <aniawaszak@yahoo.com>
2020-12-16 11:36:58 +0100

    trying to fix aws bug

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import Rewards from './components/Rewards';
 // import axios from 'axios';
 import Dashboard from './components/dashboard/Dashboard';
 import Notifications from './components/Notifications';
-import { compare } from 'bcrypt';
 
 class App extends Component {
 
