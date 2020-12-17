556f8f58e740d3afa684b71cf0a1f89dd269e488
“Anna <aniawaszak@yahoo.com>
2020-12-17 17:46:09 +0100

    split dashboard into 3 components

---
+++ client/src/components/dashboard/DashboardNoActiveChallenges.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+export default function DashboardNoActiveChallenges() {
+  return (
+    <div className="instruction-container">
+      <h2>Welcome{' '}
+        {this.props.user.firstName
+          ? this.props.user.firstName
+          : this.props.user.username} !
+      </h2>
+      <h4>You don't have any active challenges at the moment.<br/><br/>
+      Browse challenges and rewards to save your favorite ones.<br/>
+      You can also create your own! They will be automatically saved to your favorites.<br/><br/>
+      When you feel ready go to challenges, pick one and let the adventure begin!</h4>
+      
+      <div className="button-container">
+        <div className="one-button">
+        <h5>Challenges</h5>
+        <Link to="/challenges">
+          <button >BROWSE</button>
+        </Link>
+        <Link to="/challenges/create">
+          <button>CREATE</button>
+        </Link>
+        </div>
+
+        <div className="one-button">
+        <h5>Rewards</h5>
+        <Link to="/rewards">
+            <button>BROWSE</button>
+          </Link>
+          <Link to="/rewards/create">
+            <button>CREATE</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
