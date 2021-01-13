ae76f23bdd309288fa008c35688f42712341672d
“Anna <aniawaszak@yahoo.com>
2021-01-14 00:53:12 +0100

    add client and server urls to env file

--- routes/auth.js
+++ routes/auth.js
@@ -13,8 +13,8 @@ router.get('/google', passport.authenticate("google", {
 }))
 
 router.get('/google/callback', passport.authenticate('google', { 
-    successRedirect: `${process.env.SERVER_URL}/`, 
-    failureRedirect: `${process.env.SERVER_URL}/login`
+    successRedirect: `${process.env.CLIENT_URL}/`, 
+    failureRedirect: `${process.env.CLIENT_URL}/login`
     //for heroku
     // successRedirect: '/', 
     // failureRedirect: '/login'
